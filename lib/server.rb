783be8386a6dfab3353d1010003980b0c829080a
George Drummond <george@accountsapp.com>
2011-10-25 23:08:42 +0100

    dont launch the browser after, doesnt time right

--- lib/server.rb
+++ lib/server.rb
@@ -12,20 +12,10 @@ module Ghoul
           
           ###########################################################
           #
-          #   WARNING!!
-          #
           #   Using Ghoul via the "ghoul server" command is
           #   insecure by default. It is only intended for a secure
           #   local environment e.g. behind a firewall where only you
           #   or people you trust can access your machine on a lan.
-          #
-          #   We are working on getting Ghoul playing with Gitolite
-          #   so as to secure git access over ssh.
-          #
-          #   If you understand the current limitations feel free to
-          #   continue, otherwise come back another day.
-          #
-          #   Thanks, Ghoul
           #
           #############################################################
           
@@ -39,7 +29,7 @@ module Ghoul
           Rack::Server.new(:config => config_path, :Port => 3003).start
           
           # Open the users browser to our new server
-          system("open http://0.0.0.0:3003")
+          # system("open http://0.0.0.0:3003")
         rescue
           puts "  --> The ghoul shut down."
         end
